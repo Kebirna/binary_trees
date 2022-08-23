1f636314ef0cbf42c5c292e3f057c0c12cc7312f
dekenzo <79919801+Kebirna@users.noreply.github.com>
2022-08-24 03:19:56 +0800

    Update 110-binary_tree_is_bst.c

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,32 +1,70 @@
 #include "binary_trees.h"
-#include "limits.h"
+#include <stdio.h>
 
 /**
- * is_bst - helper func for binary_tree_is_bst
- * @tree: pointer to tree node
- * @min: minimum value of BST tree
- * @max: maximum value of BST tree
- * Return: true or false
+ * greater_than - check if all values in the tree are greater than a value
+ * @tree: pointer to the tree to check
+ * @val: value to check against
+ *
+ * Return: 1 if true, 0 if false
  */
-_Bool is_bst(const binary_tree_t *tree, int min, int max)
+int greater_than(const binary_tree_t *tree, int val)
 {
-	if (!tree)
-		return (true);
-	if (tree->n < min || tree->n > max)
-		return (false);
-	return (is_bst(tree->left, min, tree->n - 1) &&
-		is_bst(tree->right, tree->n + 1, max));
+	int l, r;
+
+	if (tree == NULL)
+		return (1);
+	if (tree->n > val)
+	{
+		l = greater_than(tree->left, val);
+		r = greater_than(tree->right, val);
+		if (l && r)
+			return (1);
+	}
+	return (0);
 }
 
+/**
+ * less_than - check if all values in the tree are less than a specific value
+ * @tree: pointer to the tree to check
+ * @val: value to check against
+ *
+ * Return: 1 if true, 0 if false
+ */
+int less_than(const binary_tree_t *tree, int val)
+{
+	int l, r;
+
+	if (tree == NULL)
+		return (1);
+	if (tree->n < val)
+	{
+		l = less_than(tree->left, val);
+		r = less_than(tree->right, val);
+		if (l && r)
+			return (1);
+	}
+	return (0);
+}
 
 /**
- * binary_tree_is_bst - checks if a binary tree is a valid Binary Search Tree
- * @tree: pointer to root of tree
- * Return: 1 if true 0 if false
+ * binary_tree_is_bst - checks if a binary tree is a valid binary search tree
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 1 if tree is a valid BST, and 0 otherwise. If tree is NULL, return 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	return (is_bst(tree, INT_MIN, INT_MAX));
+	if (less_than(tree->left, tree->n) && greater_than(tree->right, tree->n))
+	{
+		if (!tree->left || binary_tree_is_bst(tree->left))
+		{
+			if (!tree->right || binary_tree_is_bst(tree->right))
+				return (1);
+		}
+
+	}
+	return (0);
 }
